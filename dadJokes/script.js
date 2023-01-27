1ea26c0f539dcb791af22c3962977dc992b94637
Lucas Santos <LuSantosatl@gmail.com>
2023-01-27 14:46:33 -0500

    generate joke method created

---
+++ dadJokes/script.js
@@ -0,0 +1,36 @@
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+const jokeUrl = "https://icanhazdadjoke.com";
+
+jokeBtn.addEventListener("click", generateJoke);
+
+generateJoke();
+
+// Using async/await
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const res = await fetch(jokeUrl, config);
+
+  const data = await res.json();
+  jokeEl.innerHTML = data.joke;
+}
+
+// Using .then()
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   };
+
+//   fetch(jokeUrl, config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke;
+//     });
+// }
